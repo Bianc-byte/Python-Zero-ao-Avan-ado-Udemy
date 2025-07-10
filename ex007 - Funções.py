767d82ce1ff6a4e75c1ddf97f456a4b38253ec5a
Bianca Bueno <biancacunha326@gmail.com>
2025-07-09 22:01:59 -0300

    ex 5, 6 e 7
    adicionei mais 3 ex

---
+++ ex007 - Funções.py
@@ -0,0 +1,17 @@
+"""
+crie uma função que duplicam, triplicam e quadruplicam o número recebido como parâmetro.
+"""
+number = 1
+
+def funcao_duplica(number):
+    print(number * 2)
+
+def funcao_triplica(number):
+    print(number * 3)
+
+def funcao_quadriplica(number):
+    print(number * 4)
+
+funcao_duplica(number)
+funcao_triplica(number)
+funcao_quadriplica(number)
